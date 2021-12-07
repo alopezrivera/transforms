e79eaaa3756d0a24df260e7edd58a98f8169de4b
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-12-07 16:18:37 +0100

    Update __init__.py

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -25,11 +25,12 @@ class T:
             Describes coordinates in the **NON-ROTATED REFERENCE FRAME** from the **BODY REFERENCE FRAME**
 
         :param delta:       Rotation angle in radians
+        :param suppress:    Suppress console output (True by default)
         """
         if not isinstance(delta, type(None)):
             # Angle
             self.delta = delta
-            self.is_symbolic = 'sympy' in delta.__module__
+            self.is_symbolic = hasattr(delta, '__module__') and 'sympy' in delta.__module__
             # Parameter vector for lambdification
             self.params = [delta] if not isinstance(delta, sp.core.mul.Mul) else list(delta.free_symbols)
             # Rotation matrix creation
@@ -47,16 +48,21 @@ class T:
     def T(self):
         """
         Transform transpose
-            Describes coordinates in the **BODY REFERENCE FRAME** from the **NON-ROTATING REFERENCE FRAME**.
+            Describes coordinates in a **FINAL REFERENCE FRAME** from the **NON-ROTATING REFERENCE FRAME**.
+
+            In other words, it describes the rotation of a body from a fixed reference frame.
         :return:
         """
         return globals()[type(self).__name__](matrix=self.matrix.T, params=self.params)
 
     def Inv(self):
         """
+        As all transform matrices are orthogonal, their inverse is equal to their transpose.
+
         Transform inverse
-            Describes coordinates in the **FINAL REFERENCE FRAME** after **N LINEAR TRANSFORMATIONS** from the original
-            reference frame.
+            Describes coordinates in a **FINAL REFERENCE FRAME** from the **NON-ROTATING REFERENCE FRAME**.
+
+            IMPORTANTLY: The rotation angles are reversed.
         """
         # Transpose
         inv_matrix = self.matrix.T
@@ -95,6 +101,37 @@ class T:
                 self.params += list(sp.Matrix(other).free_symbols)
             matrix = sp.Matrix(np.matmul(self.matrix, other)) if self.is_symbolic else np.matmul(self.matrix, other)
             return T_res(matrix=matrix, params=self.params)
+
+    def __add__(self, other):
+        """
+        Addition between transforms, as well as between transforms and NumPy/SymPy arrays
+        """
+
+        T_res = globals()[type(self).__name__]
+
+        if type(other).__bases__ == type(self).__bases__:
+            # Transform-Transform multiplication
+            #       If self and other are instances of classes with the same parent class <class 'transform.T'>
+            if isinstance(self.matrix, type(other.matrix)):
+                # Both matrices are either SymPy matrices or NumPy arrays
+                matrix = self.matrix + other.matrix if self.is_symbolic else np.matmul(self.matrix, other.matrix)
+                # Right to left transform order -> same parameter order
+                params = other.params + self.params
+                return T_res(matrix=matrix, params=params)
+            else:
+                # Type mismatch: surely 1 will be symbolic, and 1 will not
+                self.type_mismatch(other)
+                matrix = sp.Matrix(self.matrix + other.matrix)
+                return T_res(matrix=matrix, params=self.params)
+        else:
+            # Transform-NumPy/SymPy array multiplication
+            if sp.Matrix(other).free_symbols:
+                self.params += list(sp.Matrix(other).free_symbols)
+            matrix = sp.Matrix(self.matrix + other) if self.is_symbolic else self.matrix + other
+            return T_res(matrix=matrix, params=self.params)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
     def __call__(self, *args):
         """
