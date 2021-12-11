4c4970dd0073fc7b0e18df13ec00dabc2cf9407b
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-12-11 19:13:19 +0100

    Install working
    Fixed issue by which pip would crete the dist-info folder of the package without actually conducting the install.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="linear_transforms",
-    version="1.0.0",
+    version="1.0.0.post1",
     author="Antonio Lopez Rivera",
     author_email="antonlopezr99@gmail.com",
     description="Python coordinate frame transform library",
